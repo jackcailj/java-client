5ef6aee6a6d5b692e7d5ea19c4b1ad4de71d632c
wybflb <wybflb@163.com>
2016-07-04 17:02:45 +0800

    ios,增加scrolltoVisible操作。

--- src/main/java/com/dangdang/reader/client/core/DeviceUtils.java
+++ src/main/java/com/dangdang/reader/client/core/DeviceUtils.java
@@ -1,14 +1,16 @@
 package com.dangdang.reader.client.core;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidKeyCode;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.pagefactory.bys.ContentMappedBy;
+import io.appium.java_client.pagefactory.bys.ContentType;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.*;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -61,5 +63,23 @@ public class DeviceUtils {
         driver.swipe(startX.intValue(),startY.intValue(),endX.intValue(),endY.intValue(),1000);
 
     }
+
+
+    /*
+    只有Ios、使用uiautomator方式时,滚动,将元素显示到屏幕上。ß
+     */
+    public static void iosScrollToVisible(AppiumDriver driver, ContentMappedBy by){
+        //ios设备,滚动到元素位置。
+        if(driver instanceof IOSDriver) {
+            ContentMappedBy uiAutomationBy = (ContentMappedBy) by;
+            By nativeBy = uiAutomationBy.getBy(ContentType.NATIVE_MOBILE_SPECIFIC);
+            if (nativeBy instanceof MobileBy.ByIosUIAutomation) {
+                logger.info("执行scrollToVisible命令");
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("UIATarget.localTarget().frontMostApp().mainWindow()" + uiAutomationBy.getByString(ContentType.NATIVE_MOBILE_SPECIFIC) + ".scrollToVisible()");
+            }
+        }
+    }
+
 
 }
